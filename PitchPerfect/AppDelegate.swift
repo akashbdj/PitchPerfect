248fad5eba7449ff8a5f1370b234cfe0e6ac1ab9
akash-bhardwaj <akashbdj@gmail.com>
2016-10-01 15:00:43 +0530

    maps ui element to code and code back to ui element

--- PitchPerfect/AppDelegate.swift
+++ PitchPerfect/AppDelegate.swift
@@ -1,10 +1,10 @@
-//
-//  AppDelegate.swift
-//  PitchPerfect
-//
-//  Created by Akash on 01/10/16.
-//  Copyright © 2016 Akash. All rights reserved.
-//
+/**
+ *  AppDelegate.swift
+ *  PitchPerfect
+ *
+ *  Created by Akash on 01/10/16.
+ *  Copyright © 2016 Akash. All rights reserved.
+ */
 
 import UIKit
 
@@ -16,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        print("Hello, World!")
         return true
     }
 
--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -1,15 +1,17 @@
-//
-//  ViewController.swift
-//  PitchPerfect
-//
-//  Created by Akash on 01/10/16.
-//  Copyright © 2016 Akash. All rights reserved.
-//
+/**
+ *  ViewController.swift
+ *  PitchPerfect
+ *
+ *  Created by Akash on 01/10/16.
+ *  Copyright © 2016 Akash. All rights reserved.
+ */
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(_ sender: AnyObject) {
+        recordingLabel.text = "Recording in progress"
+    }
 
+    @IBAction func stopRecording(_ sender: AnyObject) {
+        recordingLabel.text = "Tap to record"
+    }
 }
 
